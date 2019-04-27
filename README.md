455906f2912950b0a98229acd33a99dc01d24bd2
nashvent <anibal.ventura@ucsp.edu.pe>
2019-04-27 13:02:53 -0500

    Proyecto base

---
+++ README.md
@@ -0,0 +1,3 @@
+# Gestor de base de datos
+
+**Realizado con electron**
---
+++ main.js
@@ -0,0 +1,35 @@
+const {app, BrowserWindow} = require('electron')
+
+require('electron-reload')(__dirname, {
+  // Note that the path to electron may vary according to the main file
+  electron: require(`${__dirname}/node_modules/electron`)
+});
+
+let mainWindow
+
+function createWindow () {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  mainWindow.webContents.openDevTools();
+  mainWindow.loadFile('index.html')
+
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', function () {
+  if (mainWindow === null) createWindow()
+})
+
